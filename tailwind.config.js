1da08cfc9db5109146d0997a13424222ffad5a59
rishabharoraa <rishabharora.ns@gmail.com>
2022-08-07 13:44:39 +0530

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Playfair Display"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
